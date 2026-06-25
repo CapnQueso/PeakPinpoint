bd1baec1406dbffcf11ecd13a5d5c4e2001c0aa5
CapnQueso <ejrosas@outlook.com>
2026-06-25 03:11:28 -0700

    v0.2 uses ridge contours to determine skylines

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cv2
 import matplotlib.pyplot as plt
 
-from peakpinpoint.skyline import load_image, detect_edges, extract_skyline, draw_skyline
+from peakpinpoint.skyline import load_image, detect_edges, extract_candidate_ridges, draw_candidates
 
 image = load_image("images/motorbike.jpg")
 
 edges = detect_edges(image)
 
-skyline = extract_skyline(edges)
+#cv2.imwrite("edges.png", edges)
 
-result = draw_skyline(image, skyline)
+candidates = extract_candidate_ridges(edges)
+
+result = draw_candidates(image, candidates)
+
+print()
+for i, candidate in enumerate(candidates[:10]):
+    print(f"{i+1}: " f"score={candidate['score']:.2f}" f" x={candidate['x']}" f" y={candidate['y']}" f" w={candidate['w']}" f" h={candidate['h']}" f" area={candidate['area']:.2f}")
 
 plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
 
--- peakpinpoint/skyline.py
+++ peakpinpoint/skyline.py
@@ -16,20 +16,47 @@ def detect_edges(image):
     edges = cv2.Canny(blurred, 50, 150)
     return edges
 
-def extract_skyline(edges):
+# def extract_skyline(edges):
+#     height, width = edges.shape
+#     skyline = []
+
+#     for x in range(width):
+#         column = edges[:, x]
+#         # find edge pixels
+#         edge_locations = np.where(column > 0)[0]
+
+#         if len(edge_locations) > 0:
+#             skyline.append(edge_locations[0])
+#         else:
+#             skyline.append(height)
+#     return np.array(skyline)
+
+def extract_candidate_ridges(edges):
+    # finds candidate ridge contours and returns a list of said contours
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+    candidates = []
+
     height, width = edges.shape
-    skyline = []
 
-    for x in range(width):
-        column = edges[:, x]
-        # find edge pixels
-        edge_locations = np.where(column > 0)[0]
+    for contour in contours:
+        area = cv2.contourArea(contour)
 
-        if len(edge_locations) > 0:
-            skyline.append(edge_locations[0])
-        else:
-            skyline.append(height)
-    return np.array(skyline)
+        x, y, w, h = cv2.boundingRect(contour)
+
+        score = w * 2 - y + area * 0.01
+
+        candidates.append({"contour": contour, "score": score, "x": x, "y": y, "w": w, "h": h, "area": area})
+    
+    candidates.sort(key=lambda c: c["score"], reverse=True)
+
+    return candidates[:20]
+
+def draw_candidates(image, candidates):
+    output = image.copy()
+    for candidate in candidates:
+        cv2.drawContours(output, [candidate["contour"]], -1, (0, 0, 255), 2)
+    return output
 
 def draw_skyline(image, skyline):
     output = image.copy()
